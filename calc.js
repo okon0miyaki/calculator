97d921ca03a93ff9696165654443d331add162ef
RnG <113661821+okon0miyaki@users.noreply.github.com>
2024-09-12 20:37:43 +0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,27 @@
+function jumlah() {
+                var a = parseInt(document.getElementById('1').value);
+                var b = parseInt(document.getElementById('2').value);
+                var hasil = a + b;
+                alert("Hasil: " + hasil);
+            }
 
+            function kurang() {
+                var a = parseInt(document.getElementById('1').value);
+                var b = parseInt(document.getElementById('2').value);
+                var hasil = a - b;
+                alert("Hasil: " + hasil);
+            }
+
+            function kali() {
+                var a = parseInt(document.getElementById('1').value);
+                var b = parseInt(document.getElementById('2').value);
+                var hasil = a * b;
+                alert("Hasil: " + hasil);
+            }
+
+            function bagi() {
+                var a = parseInt(document.getElementById('1').value);
+                var b = parseInt(document.getElementById('2').value);
+                var hasil = a / b;
+                alert("Hasil: " + hasil);
+            }
